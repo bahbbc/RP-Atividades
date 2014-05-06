f27efa8ef26222d1c12d76b520e6688ef5e4e14a
Barbara Barbosa <barbarab.claudino@gmail.com>
2014-05-05 22:44:10 -0300

    Um codigo mais bonito, datasets e deletado arquivo maluco

--- atividade_6.r
+++ atividade_6.r
@@ -1,23 +1,31 @@
-function split(data, k){
-	split = flor(data.length / k)
-	left = data.length %% k
+split_data = function(data, k){
+	split = floor(nrow(data)/k)
+	left = nrow(data) %% k
+	sizes = 0
 	# por enquanto soh diz quanto vai ter em cada subconjunto
 	#acredito que temos que dividir o conj. original, soh num sei se eh agora ou na prox. fase
 	for(j in 1:k){
 		sizes[j] = split
 	}
 
-	for(i in 1:length(left)){
+	for(i in 1:left){
 		 #substituir essa soma por rbind()
-		 sizes[i] = sizes [i]+1
+		 sizes[i] = sizes[i]+1
 	}
+	return(sizes)
+}
 
+data_proportion = function(data, k){
 	#manter as proporcoes das classes originais.
 	#separa por classe
 	class = data[,1] #supoem-se que a primeira coluna eh a classe
 	data_by_class = split(data, class)
-	for(k in 1:length(data_by_class)){
-		tam[k] = data_by_class[k]
-		#acho q tem um jeito mais esperto de contar, mas eu toh com mt sono.
-	}
+	#nao sei trabalhar com listas ai joguei td no data frame
+	data_attr1 = data.frame(data_by_class[1])
+	data_attr2 = data.frame(data_by_class[2])
+	total = split_data(data, k)
+	attr1 = split_data(data_attr1, k)
+	attr2 = split_data(data_attr2, k)
+	split_data = data.frame(total, attr1, attr2)
+	return(split_data)
 }
